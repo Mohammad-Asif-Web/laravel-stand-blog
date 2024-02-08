d4747fe15990bd2e05908f2e5af067e0773de325
muhammad asif <alexasifhhossain@gmail.com>
2024-02-08 17:53:21 +0600

    custom css added

--- resources/views/backend/includes/head.blade.php
+++ resources/views/backend/includes/head.blade.php
@@ -6,4 +6,5 @@
 <title> @yield('title') | Admin</title>
 <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
 <link href="{{asset('backend/css/styles.css')}}" rel="stylesheet" />
+<link href="{{asset('backend/css/custom.css')}}" rel="stylesheet">
 <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
