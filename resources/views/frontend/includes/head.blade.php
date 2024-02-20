dc721e1afb95687b700cdc202b85babe398173fa
muhammad asif <alexasifhhossain@gmail.com>
2024-02-20 21:02:34 +0600

    custom css link added

--- resources/views/frontend/includes/head.blade.php
+++ resources/views/frontend/includes/head.blade.php
@@ -9,5 +9,6 @@
 <link href="{{asset('frontend/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
 <!-- Additional CSS Files -->
 <link rel="stylesheet" href="{{asset('frontend/assets/css/fontawesome.css')}}">
+<link rel="stylesheet" href="{{asset('frontend/assets/css/custom.css')}}">
 <link rel="stylesheet" href="{{asset('frontend/assets/css/templatemo-stand-blog.css')}}">
 <link rel="stylesheet" href="{{asset('frontend/assets/css/owl.css')}}">
