145e5fe109dc6e9bb05f6db2b6856caa7d175009
muhammad asif <alexasifhhossain@gmail.com>
2024-03-10 13:38:09 +0600

    login password show and hide system added

--- resources/views/backend/auth/login.blade.php
+++ resources/views/backend/auth/login.blade.php
@@ -6,11 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login Form</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" rel="stylesheet" />    
+    <link href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" rel="stylesheet" />
     <link rel="stylesheet" href="{{asset('auth/login.css')}}">
 </head>
 <body>
-    
+
     <div class="wrapper bg-white">
         <div class="h2 text-center">Dashboard</div>
         <div class="h4 text-muted text-center pt-2">Enter your login details</div>
@@ -20,9 +20,9 @@
             <div class="form-group py-2">
                 <div class="{{$errors->has('email') ? 'is-invalid input-field form-control': 'input-field'}}">
                     <span class="far fa-user p-2"></span>
-                    <input type="email" name="email" 
-                        placeholder="Email Address" required 
-                        class="">
+                    <input type="email" name="email"
+                        placeholder="Email Address"
+                        class="" required>
                 </div>
             </div>
             @error('email')
@@ -32,11 +32,11 @@
             <div class="form-group py-1 pb-2">
                 <div class="{{$errors->has('password') ? 'is-invalid input-field form-control': 'input-field'}}">
                     <span class="fas fa-lock p-2"></span>
-                    <input type="password" name="password" type="text" 
-                            placeholder="Enter your Password" required class="">
-                    <button class="btn bg-white text-muted">
-                        <span class="far fa-eye-slash"></span>
-                    </button>
+                    <input type="password" name="password" type="text" id="password" class=""
+                            placeholder="Enter your Password" required>
+                    <a class="btn bg-white text-muted">
+                        <span style="color: #400485;" class="far fa-eye-slash" id="eye"></span>
+                    </a>
                 </div>
             </div>
             @error('password')
@@ -64,6 +64,27 @@
 
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" crossorigin="anonymous"></script>
+
+    <script>
+        const passwordInput = $('#password');
+        const icon = $('#eye');
+        let handle = false;
+        icon.on('click', function(){
+            if (handle == true) {
+                passwordInput.attr('type', 'password');
+                icon.removeClass("far fa-eye");
+                icon.addClass("far fa-eye-slash");
+                handle = false;
+            } else {
+                passwordInput.attr('type', 'text');
+                icon.removeClass("far fa-eye-slash");
+                icon.addClass("far fa-eye");
+                handle = true;
+            }
+            // console.log('clicked');
+
+        })
+    </script>
 
 </body>
 </html>
